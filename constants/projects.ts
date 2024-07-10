98f19024a95e2f7c57604f51e12e47f8bd44e6f6
Avinash-1-10 <avinashyede110@gmail.com>
2024-07-10 17:07:41 +0530

    add new project

--- constants/projects.ts
+++ constants/projects.ts
@@ -8,35 +8,43 @@ interface Project {
 }
 
 const projects: Project[] = [
-    {
-      name: "MyTrip - Ticket Booking Platform",
-      description:
-        "MyTrip is a comprehensive ticket booking platform where users can search for trips, select seats, and make payments using credit cards or UPI.",
-      image:
-        "https://res.cloudinary.com/dfqd02z8w/image/upload/v1713334062/mddim4kthehusdqi6xcx.jpg",
-      code: "https://github.com/Avinash-1-10/mytrip",
-      tech: ["React.js", "Node.js", "MongoDB", "Material UI"],
-    },
-    {
-      name: "Calendly - Meeting Scheduler",
-      description:
-        "Calendly simplifies the process of arranging and managing meetings. Users can create events, share links, select time slots, send email notifications, and more.",
-      image:
-        "https://res.cloudinary.com/dfqd02z8w/image/upload/v1713334378/evj0jouhzfj1wxqjvpcr.jpg",
-      code: "https://github.com/Avinash-1-10/meeting-scheduler",
-      tech: ["Next.js", "Firebase", "Shadcn", "Tailwind"],
-    },
-    {
-      name: "Resume Builder",
-      description:
-        "Resume Builder is a user-friendly platform where users can effortlessly create resumes by filling out a form. Resumes can be downloaded in PDF format.",
-      image:
-        "https://res.cloudinary.com/dfqd02z8w/image/upload/v1713334727/sw1nqdqz6d2hzzjqxoq3.jpg",
-      code: "https://github.com/Avinash-1-10/resume-builder-project?tab=readme-ov-file",
-      demo: "https://resume-builder-project-mauve.vercel.app",
-      tech: ["React.js", "Material UI", "Redux", "useForm"],
-    },
-    
-  ];
+  {
+    name: "Social Media App",
+    description:
+      "Social Media App is a social media platform where users can create posts, share photos, and connect with other users. Users can also follow, unfollow, and comment on posts.",
+    image:
+      "https://res.cloudinary.com/dfqd02z8w/image/upload/v1720611260/gavlh2opno7reiv9ilmo.jpg",
+    code: "https://github.com/Avinash-1-10/threads",
+    tech: ["React.js", "Node.js", "MongoDB", "Chakra UI"],
+  },
+  {
+    name: "MyTrip - Ticket Booking Platform",
+    description:
+      "MyTrip is a comprehensive ticket booking platform where users can search for trips, select seats, and make payments using credit cards or UPI.",
+    image:
+      "https://res.cloudinary.com/dfqd02z8w/image/upload/v1713334062/mddim4kthehusdqi6xcx.jpg",
+    code: "https://github.com/Avinash-1-10/mytrip",
+    tech: ["React.js", "Node.js", "MongoDB", "Material UI"],
+  },
+  {
+    name: "Calendly - Meeting Scheduler",
+    description:
+      "Calendly simplifies the process of arranging and managing meetings. Users can create events, share links, select time slots, send email notifications, and more.",
+    image:
+      "https://res.cloudinary.com/dfqd02z8w/image/upload/v1713334378/evj0jouhzfj1wxqjvpcr.jpg",
+    code: "https://github.com/Avinash-1-10/meeting-scheduler",
+    tech: ["Next.js", "Firebase", "Shadcn", "Tailwind"],
+  },
+  {
+    name: "Resume Builder",
+    description:
+      "Resume Builder is a user-friendly platform where users can effortlessly create resumes by filling out a form. Resumes can be downloaded in PDF format.",
+    image:
+      "https://res.cloudinary.com/dfqd02z8w/image/upload/v1713334727/sw1nqdqz6d2hzzjqxoq3.jpg",
+    code: "https://github.com/Avinash-1-10/resume-builder-project?tab=readme-ov-file",
+    demo: "https://resume-builder-project-mauve.vercel.app",
+    tech: ["React.js", "Material UI", "Redux", "useForm"],
+  },
+];
 
-  export default projects
+export default projects;
